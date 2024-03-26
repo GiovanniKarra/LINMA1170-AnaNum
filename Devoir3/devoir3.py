f4c196fe94db2757af42f0a9b5e35fcc0038462c
Giovanni Karra <karragiovanni@gmail.com>
2024-03-26 11:45:28 +0100

    début algos

--- Devoir3/devoir3.py
+++ Devoir3/devoir3.py
@@ -1,33 +1,54 @@
 import numpy as np
+import scipy.linalg as sp
 import numba as nb
 import matplotlib.pyplot as plt
 from time import perf_counter
 
 
-def hessenberg(self, A, Q):
-    n = A.shape[0]
-    self.labels[2].setText("Optional label")
-    self.set_delay(1)
-    for i in range(n):
-        for j in range(n):
-            self.set_matrix(A, i, j, A[i, j])
-    Q[0, 0] = 1.0
-    return
+#@nb.jit(nopython=True)
+def hessenberg(A, Q):
+	n = np.shape(A)[0]
+	for i in range(n-2):
+		x = A[i+1:n, i][np.newaxis].T
+		x[0, 0] += np.sqrt(x.conjugate().T@x)[0, 0]*np.sign(x[0, 0])
+		x /= np.sqrt(x.conjugate().T@x)
 
-def step_qr(self, H, Q, m):  
-    n = H.shape[0]
-    tmp = np.copy(np.diag(H))
-    for i in range(n):
-        self.set_matrix(H, i, i, 0.95*tmp[i] + 0.05*tmp[i-2].imag + 0.05*1j*tmp[i-1].real)
-    return
+		A[i+1:n, i:n] -= 2*x @ (x.conjugate().T@A[i+1:n, i:n])
+		A[:n, i+1:n] -= 2*(A[:n, i+1:n]@x)@x.conjugate().T
+		
+		# un peu chelou
+		v = np.array([x[j-i-1, 0] if (j > i and j < n) else 0 for j in range(n)], dtype="complex")[np.newaxis].T
+		Q[i+1:n, i:n] @= np.identity(n,dtype="complex") - 2 * v @ v.conjugate().T
 
-def step_qr_shift(self, H, Q, m):
-    self.step_qr(H, Q, m)
-    return m
+def givens(a, b):
+	return 0, 0
+
+def step_qr(H, Q, m):
+	n = np.shape(A)[0]
+	c = np.empty(n-1, dtype="complex")
+	s = np.empty(n-1, dtype="complex")
+	for i in range(n-1):
+		c[i], s[i] = givens(H[i, i], H[i+1, i])
+		H[i:i+1, i:n] = np.array([[c[i], -s[i]], [s[i], c[i]]], dtype="complex")@H[i:i+1, i:n]
+
+	for i in range(n-1):
+		H[:i+1, i:i+1] @= np.array([[c[i], s[i]], [-s[i], c[i]]], dtype="complex")
+
+def step_qr_shift(H, Q, m):
+	
+	return m
 
 def solve_qr(A, use_shifts):
-    return A
+	return A
 
 
 if __name__ == "__main__":
-    pass
+	A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype="complex")
+	# A = np.random.rand(5, 5) * 10
+	n = np.shape(A)[0]
+	Q = np.empty((n, n), dtype="complex")
+
+	hessenberg(A, Q)
+	print(A)
+	print(Q)
+	# print(sp.hessenberg(A))
