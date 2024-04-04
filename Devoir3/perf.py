8b9e7c993724c76fdd93f509a318837bdbd9318e
Giovanni Karra <karragiovanni@gmail.com>
2024-04-04 20:32:32 +0200

    code pas encore 100% fini mais début d'analyse de perfs

--- Devoir3/perf.py
+++ Devoir3/perf.py
@@ -1,12 +1,87 @@
 import scipy.linalg as sp
 import numpy as np
 import matplotlib.pyplot as plt
+from time import perf_counter
 
 from devoir3 import solve_qr
 
-def complexity():
-	pass
+def plot_complexity(min, max, num, eps, max_iter):
+	size = np.logspace(np.log10(min), np.log10(max), num)
+	exec_time = np.empty(num, dtype=np.float64)
+	exec_time_shift = np.empty(num, dtype=np.float64)
+	exec_time_scipy = np.empty(num, dtype=np.float64)
+	iter_num = np.empty(num, dtype=int)
+	iter_num_shift = np.empty(num, dtype=int)
+
+	for i in range(num):
+		n = int(size[i])
+		A = np.asarray(np.random.rand(n, n) * 100, dtype="complex") +\
+			np.asarray(np.random.rand(n, n) * 100, dtype="complex")*1j
+
+		time = perf_counter()
+		_, k = solve_qr(A.copy(), False, eps, max_iter)
+		exec_time[i] = perf_counter()-time
+
+		time = perf_counter()
+		_, k2 = solve_qr(A.copy(), True, eps, max_iter)
+		exec_time_shift[i] = perf_counter()-time
+
+		time = perf_counter()
+		_ = sp.schur(A, output="complex")
+		exec_time_scipy[i] = perf_counter()-time
+
+		if k == -1: k = max_iter
+		if k2 == -1: k2 = max_iter
+
+		iter_num[i] = k
+		iter_num_shift[i] = k2
+
+		print("iteration %d/%d (size %d) done !" % (i, num, n))
+
+
+	plt.figure()
+
+	plt.title("Complexity analysis of the QR Algorithm")
+
+	plt.xlabel("matrix size")
+	plt.ylabel("execution time [s]")
+
+	plt.loglog(size, exec_time)
+	plt.loglog(size, exec_time_shift)
+	plt.loglog(size, exec_time_scipy)
+	plt.loglog(size, 1e-9*size**3, linestyle="dashed")
+
+	plt.grid(which="major", linestyle="-")
+	plt.grid(which="minor", linestyle=":")
+
+	plt.legend(["QR Algorithm (no shifts)", "QR Algorithm", "scipy.linalg.schur",
+				"$\mathcal{O}(m^3)$"])
+
+	plt.show()
+	# plt.savefig("rapport/images/complcomp.svg", format="svg")
+
+	plt.figure()
+
+	plt.title("Number of iterations before convergence")
+
+	plt.xlabel("matrix size")
+	plt.ylabel("Iteration count")
+
+	plt.semilogx(size, iter_num)
+	plt.semilogx(size, iter_num_shift)
+	plt.semilogx(size, np.ones(num)*max_iter, linestyle="dashed")
+
+	plt.grid(which="major", linestyle="-")
+	plt.grid(which="minor", linestyle=":")
+
+	plt.legend(["No shift", "With shift", "Max iteration allowed"])
+
+	plt.show()
+	# plt.savefig("rapport/images/complratio.svg", format="svg")
 
 
 if __name__ == "__main__":
-	pass
+	# pour compiler
+	solve_qr(np.array([[2, 1, 1], [1, 2, 1], [1, 1, 2]], dtype="complex"), True, 1e-12, 10)
+
+	plot_complexity(10, 1000, 5, 1e-12, 1000)
