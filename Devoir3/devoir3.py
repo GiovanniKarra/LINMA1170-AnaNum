7cd77eda50779b3cc40b2b90571221eb3a18499d
Giovanni Karra <karragiovanni@gmail.com>
2024-04-04 17:51:01 +0200

    plus que le shift de merde à regler

--- Devoir3/devoir3.py
+++ Devoir3/devoir3.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy.linalg as sp
 import numba as nb
 
+import numba as nb
+import numpy as np
 
 @nb.jit(nopython=True)
 def mult(A, B):
@@ -53,6 +55,27 @@ def to_matrix(a):
 
 
 @nb.jit(nopython=True)
+def round_matrix(A, eps):
+	m, n = np.shape(A)
+	for i in range(m):
+		for j in range(n):
+			if A[i, j].imag < eps:
+				A[i, j] = complex(A[i, j].real, 0)
+			if A[i, j].real < eps:
+				A[i, j] = complex(0, A[i, j].imag)
+
+
+@nb.jit(nopython=True)
+def subdiag_small(A, eps):
+	m = np.shape(A)[0]
+	for i in range(1, m):
+		if np.abs(A[i, i-1]) > eps:
+			return False
+			
+	return True
+
+
+@nb.jit(nopython=True)
 def hessenberg(A, Q):
 	n = np.shape(A)[0]
 	v = np.copy(A)
@@ -82,56 +105,50 @@ def hessenberg(A, Q):
 
 @nb.jit(nopython=True)
 def givens(a, b):
+	phi = np.angle(b)-np.angle(a)
+	comp = np.exp(1j*phi)
 	return np.abs(a)/np.sqrt(np.abs(a)**2 + np.abs(b)**2),\
-		-np.abs(b)/np.sqrt(np.abs(a)**2 + np.abs(b)**2)
+		np.abs(b)/np.sqrt(np.abs(a)**2 + np.abs(b)**2), comp
 
 
 @nb.jit(nopython=True)
 def step_qr(H, Q, m):
 	c = np.empty(m-1, dtype="complex")
 	s = np.empty(m-1, dtype="complex")
+	t = np.empty(m-1, dtype="complex")
 	for i in range(m-1):
-		c[i], s[i] = givens(H[i, i], H[i+1, i])
-		H[i:i+2, i:m] = mult(np.array([[c[i], -s[i]], [s[i], c[i]]], dtype="complex"),
-			H[i:i+2, i:m])
+		c[i], s[i], t[i] = givens(H[i, i], H[i+1, i])
+		g_star = np.array([[c[i], s[i]*np.conj(t[i])],
+					 	   [-s[i]*t[i], c[i]]], dtype="complex")
+		H[i:i+2, i:m] = mult(g_star, H[i:i+2, i:m])
 
 	for i in range(m-1):
-		g = np.array([[c[i], s[i]], [-s[i], c[i]]], dtype="complex")
+		g = np.array([[c[i], -s[i]*np.conj(t[i])],
+					  [s[i]*t[i], c[i]]], dtype="complex")
 		H[:i+2, i:i+2] = mult(H[:i+2, i:i+2], g)
 
 		Q[:m, i:i+2] = mult(Q[:m, i:i+2], g)
 
-	round_matrix(H, 1e-15)
-	round_matrix(Q, 1e-15)
-
 
 @nb.jit(nopython=True)
-def step_qr_shift(H, Q, m, eps, max_iter=float("inf")):
-	k = 0
-	while np.abs(H[m-1, m-2]) > eps and float(k) < max_iter:
-		delta = (H[m-2, m-2]-H[m-1, m-1])/2
-		b_sq = H[m-1, m-2]**2
-		denom = np.abs(delta)*np.sqrt(delta**2+b_sq)
-		shift = H[m-1, m-1]-sign(delta, eps)*b_sq/(denom if np.abs(denom) > eps else 1)
-
-		for i in range(m):
-			H[i, i] -= shift
-		step_qr(H, Q, m)
-		for i in range(m):
-			H[i, i] += shift
-
-		k += 1
-
-	return m-1
-
+def wilkinson_shift(B):
+	a = B[0, 0]; b = B[0, 1]; c = B[1, 0]; d = B[1, 1]
+	sqrt_rho = np.sqrt(a**2 + d**2 + 4*b*c - 2*a*d)
+	aplusd = a+d
+	l1 = (aplusd-sqrt_rho)/2; l2 = (aplusd+sqrt_rho)/2
+	return l1 if np.abs(l1-d) < np.abs(l2-d) else l2
 
 @nb.jit(nopython=True)
-def round_matrix(A, eps):
-	m, n = np.shape(A)
+def step_qr_shift(H, Q, m, eps):
+	shift = wilkinson_shift(H[m-2:m, m-2:m])
+
 	for i in range(m):
-		for j in range(n):
-			if np.abs(A[i, j]) < eps:
-				A[i, j] = 0
+		H[i, i] -= shift
+	step_qr(H, Q, m)
+	for i in range(m):
+		H[i, i] += shift
+
+	return m-(1 if np.abs(H[m-1, m-2]) < eps else 0)
 
 
 @nb.jit(nopython=True)
@@ -143,17 +160,18 @@ def solve_qr(A, use_shifts, eps, max_iter):
 
 	if use_shifts:
 		m = n
-		while m > 1:
-			m = step_qr_shift(A, Q, m, eps, max_iter*10)
+		for i in range(max_iter):
+			if m > 1:
+				m = step_qr_shift(A, Q, m, eps)
+			else:
+				k = i
+				break
 	else:
 		for i in range(max_iter):
 			step_qr(A, Q, n)
-			if np.all(np.abs(np.diag(A)) < eps):
+			if subdiag_small(A, eps):
 				k = i
 				break
-	
-	round_matrix(A, eps)
-	round_matrix(Q, eps)
 
 	return Q, k
 
@@ -162,17 +180,17 @@ if __name__ == "__main__":
 	np.set_printoptions(precision=1)
 
 	# A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype="complex")
-	A = np.array([[1+1j, 2+8j, 3+6j], [4, 5+3j, 6], [7+7j, 8, 9]], dtype="complex")
+	# A = np.array([[1+1j, 2+8j, 3+6j], [4, 5+3j, 6], [7+7j, 8, 9]], dtype="complex")
 	# A = np.array([[1+1j]], dtype="complex")
-	# A = np.asarray(np.random.rand(5, 5) * 10, dtype="complex") +\
-	# 	np.asarray(np.random.rand(5, 5) * 10, dtype="complex")*1j
+	A = np.asarray(np.random.rand(5, 5) * 10, dtype="complex") +\
+		np.asarray(np.random.rand(5, 5) * 10, dtype="complex")*1j
 	B = A.copy()
 	n = np.shape(A)[0]
 	
 	
 	Q = np.empty((n, n), dtype="complex")
-	# Q, k = solve_qr(A, True, 1e-12, 1000)
-	Q, k = solve_qr(A, False, 1e-12, 1000)
+	Q, k = solve_qr(A, True, 1e-12, 1000)
+	# Q, k2 = solve_qr(A, False, 1e-12, 1000)
 	# hessenberg(A, Q)
 	# round_matrix(A, 1e-12)
 
@@ -180,4 +198,5 @@ if __name__ == "__main__":
 	print(np.allclose(B, mult(mult(Q, A), Q.conjugate().T)))
 	print(mult(Q, Q.conjugate().T))
 	# print(A)
+	# print(sp.schur(B, output='complex')[0])
 	# print(mult(mult(Q, A), Q.conjugate().T))
