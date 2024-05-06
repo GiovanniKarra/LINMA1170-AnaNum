9482f38417c8c1c51381219bf5f64e0387e4cd63
Giovanni Karra <karragiovanni@gmail.com>
2024-05-06 23:30:21 +0200

    début exploration

--- Devoir4/code/main.py
+++ Devoir4/code/main.py
@@ -1,4 +1,15 @@
 import numpy as np
 from numpy.linalg import svd
+import networkx as nx
+
+from graph_utils import get_adj
+from graph_visu import plot_graph
 
 
+if __name__ == "__main__":
+	G = nx.random_geometric_graph(5, 0.7)
+
+	plot_graph(G)
+
+	A = get_adj(G)
+	print(A)
